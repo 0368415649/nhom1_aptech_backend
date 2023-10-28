810712b283eea6c7c33db682ca87c6a6f14482b1
nguyendinhduy <dinhduy2k1312@gmail.com>
2023-10-28 14:46:37 +0700

    commit code

--- api_source/backend/backend/backend/Global.asax.cs
+++ api_source/backend/backend/backend/Global.asax.cs
@@ -25,7 +25,7 @@ namespace backend
             HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
             if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
             {
-                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
                 HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
                 HttpContext.Current.Response.StatusCode = 200;
                 HttpContext.Current.Response.End();
