3477500d09b820d068e46595538b3131ce2f6b35
nguyendinhduy <dinhduy2k1312@gmail.com>
2023-11-16 12:57:23 +0700

    COMMIT CODE

---
+++ api_source/backend/backend/backend/Controllers/feebacksController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using backend.Models;
+
+namespace backend.Controllers
+{
+    public class feebacksController : ApiController
+    {
+        private ADDDA_APPEntities db = new ADDDA_APPEntities();
+
+        // GET: api/feebacks
+        public IQueryable<feeback> Getfeeback()
+        {
+            return db.feeback;
+        }
+
+        [ResponseType(typeof(address))]
+        [HttpPost]
+        [Route("api/create_feeback")]
+        [ResponseType(typeof(feeback))]
+        public IHttpActionResult Postfeeback(feeback feeback)
+        {
+            try
+            {
+                DateTime currentDate = DateTime.Now;
+                feeback.create_at = currentDate.ToString("yyyy-MM-dd HH:mm:ss");
+                db.feeback.Add(feeback);
+                db.SaveChanges();
+                return Ok(new { Status = 1 });
+            }
+            catch (Exception)
+            {
+                return Ok(new { Status = 0 });
+                throw;
+            }
+            
+
+            return CreatedAtRoute("DefaultApi", new { id = feeback.feeback_id }, feeback);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool feebackExists(int id)
+        {
+            return db.feeback.Count(e => e.feeback_id == id) > 0;
+        }
+    }
+}
---
+++ api_source/backend/backend/backend/Service/SeviceInterface/ICarServer.cs
@@ -0,0 +1,25 @@
+﻿using backend.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace backend.Service.SeviceInterface
+{
+    public interface ICarServer
+    {
+        IEnumerable<car_view> GetAllTableCar();
+        IEnumerable<car_view> GetAllCar(int? typeCar, int? brand, string order_by_price, string name);
+        IEnumerable<car_view> GetFavoriteCar(int? customer_id);
+        IEnumerable<car_view> GetAllMyCar(int? customer_id);
+        IEnumerable<car_view> Getcar(int id);
+        Result ChangeStatusCar(car car);
+        Task<Result> Postcar(MultipartFormDataStreamProvider fileData);
+
+    }
+}
